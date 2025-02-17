0471aa27747f65976c8df3a1337d153af46df4c4
Speedy_Lex <alex.ciocildau@gmail.com>
2025-02-17 20:30:32 +0100

    polish drawing applet

--- mnist/src/bin/draw.rs
+++ mnist/src/bin/draw.rs
@@ -4,6 +4,8 @@ use convoluted::{array::Array1D, cost::CrossEntropy, layer::{dense::DenseLayer, 
 
 type Network = convoluted::Network<Array1D<{ 28*28 }>, LayerChain<LayerChain<LayerChain<LayerChain<DenseLayer<{ 28*28 }, 100>, (), Array1D<{ 28*28 }>>, SigmoidLayer<100>, Array1D<{ 28*28 }>>, DenseLayer<100, 10>, Array1D<{ 28*28 }>>, SigmoidLayer<10>, Array1D<{ 28*28 }>>, CrossEntropy, Array1D<10>, usize>;
 
+const PIXEL_SIZE: usize = 20;
+
 fn main() {
     let mut network = Network::load("network.bin").unwrap();
     let (mut rl, rt) = init()
@@ -11,28 +13,19 @@ fn main() {
     .fullscreen()
     .build();
 
+    let width = rl.get_screen_width() as usize;
+    let height = rl.get_screen_height() as usize;
+
+    rl.set_target_fps(60);
+
     let mut drawing_area = Array1D::new();
     while !rl.window_should_close() {
         if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_LEFT) {
             let mut pos = rl.get_mouse_position();
-            pos /= 20.0; // div by pixel size
+            pos -= Vector2::new(width as f32 / 2.0, height as f32 / 2.0);
+            pos += Vector2::new(14.0 * PIXEL_SIZE as f32, 14.0 * PIXEL_SIZE as f32);
+            pos /= PIXEL_SIZE as f32; // div by pixel size
             paintbrush(drawing_area.as_mut_slice(), pos.x, pos.y);
-            // if !(pos.x >= 28.0 || pos.y >= 28.0) {
-            //     for (x_offset, y_offset) in [(0, 0), (1, 0), (0, 1), (1, 1)] {
-            //         let mut x = pos.x.ceil() as usize + x_offset;
-            //         if x == 0 {
-            //             continue;
-            //         }
-            //         x -= 1;
-            //         let mut y = pos.y.ceil() as usize + y_offset;
-            //         if y == 0 {
-            //             continue;
-            //         }
-            //         y -= 1;
-            //         let idx = x + y * 28;
-            //         drawing_area[idx] = drawing_area[idx].max(Vector2::new(x as f32, y as f32).distance_to(pos));
-            //     }
-            // }
         }
         if rl.is_key_pressed(KeyboardKey::KEY_DELETE) {
             drawing_area = Array1D::new();
@@ -44,8 +37,8 @@ fn main() {
 
         for y in 0..28 {
             for x in 0..28 {
-                let col = (drawing_area[x + 28*y] * 256.0).floor() as u8;
-                d.draw_rectangle((x*20) as i32, (y*20) as i32, 20, 20, Color::new(col, col, col, 255));
+                let col = 255 - (drawing_area[x + 28*y] * 256.0).floor() as u8;
+                d.draw_rectangle((x * PIXEL_SIZE) as i32 + width as i32/2 - 14 * PIXEL_SIZE as i32, (y * PIXEL_SIZE) as i32 + height as i32/2 - 14 * PIXEL_SIZE as i32, PIXEL_SIZE as i32, PIXEL_SIZE as i32, Color::new(col, col, col, 255));
             }
         }
 
@@ -53,14 +46,13 @@ fn main() {
         sorted.sort_by(|x, y| y.1.partial_cmp(x.1).unwrap());
         let sum: f32 = sorted.iter().map(|x| x.1).sum();
         for (i, (number, chance)) in sorted.iter().enumerate() {
-            d.draw_text(&format!("{}: {:.2}%", number, *chance / sum * 100.0), 1000, 40 * i as i32, 30, Color::WHITE);
+            d.draw_text(&format!("{}: {:.2}%", number, *chance / sum * 100.0), width as i32 / 2 + 14 * PIXEL_SIZE as i32 + 20, 40 * i as i32 + height as i32 / 2 - 40 * 5 + 5, 30, Color::WHITE);
         }
-        d.draw_fps(10, 10);
     }
 }
 
 fn paintbrush(array: &mut [f32], pos_x: f32, pos_y: f32) {
-    let radius = 1.0;
+    let radius = 1.5;
     let samples = 4; // Increase for better antialiasing
     let step = 1.0 / (samples as f32);
     
