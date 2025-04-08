2589e2d1a571abe353d424b26a82b07105839b92
Speedy-Lex <cli+speedy-lex@users.noreply.github.com>
2025-04-08 13:39:52 +0200

    remove trailing whitespace in mnist csv parser

--- mnist/src/lib.rs
+++ mnist/src/lib.rs
@@ -11,7 +11,7 @@ pub fn get_mnist_train() -> (Vec<Array1D<{ 28*28 }>>, Vec<usize>) {
     let mut labels = Vec::with_capacity(60_000);
     let mut n = 0;
     let mut src = rand::rng();
-    for x in fs::read_to_string("src/mnist_train.csv").unwrap().split('\n') {
+    for x in fs::read_to_string("src/mnist_train.csv").unwrap().trim_end().split('\n') {
         labels.push(x[0..1].parse::<usize>().unwrap());
         data.push(Array1D::new());
         for (i, pixel) in x.split(',').skip(1).enumerate() {
@@ -30,7 +30,7 @@ pub fn get_mnist_test() -> (Vec<Array1D<{ 28*28 }>>, Vec<usize>) {
     let mut data = Vec::with_capacity(10_000);
     let mut labels = Vec::with_capacity(10_000);
     let mut n = 0;
-    for x in fs::read_to_string("src/mnist_test.csv").unwrap().split('\n') {
+    for x in fs::read_to_string("src/mnist_test.csv").unwrap().trim_end().split('\n') {
         labels.push(x[0..1].parse::<usize>().unwrap());
         data.push(Array1D::new());
         for (i, pixel) in x.split(',').skip(1).enumerate() {
